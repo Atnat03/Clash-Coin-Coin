763e17cb585eeb9da717bd327e7d1972ce30b94c
Abal798 <b.berger@rubika-edu.com>
2026-01-19 17:01:29 +0100

    inputSysytem

---
+++ Assets/scripts/MashingGame/MashIngManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MashIngManager : MonoBehaviour
+{
+    public float P1JaugeFillAmout = 1f;
+    public Image P1Jauge;
+    
+    public float P2JaugeFillAmout = 1f;
+    public Image P2Jauge;
+    
+    
+    public IEnumerator 
+}
