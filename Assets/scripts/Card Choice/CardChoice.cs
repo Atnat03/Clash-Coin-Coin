028a8a7f9e431b76421b03f1f937d5665b5474c8
Entity02 <114674519+Entity02@users.noreply.github.com>
2026-01-20 13:38:53 +0100

    c'est cool

--- Assets/scripts/Card Choice/CardChoice.cs
+++ Assets/scripts/Card Choice/CardChoice.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CardChoice : MonoBehaviour
@@ -20,7 +21,81 @@ public class CardChoice : MonoBehaviour
    public bool player1Fail, player2Fail;
 
    public int[] cardsID1, cardsID2;
+   
+   public int cardSelected1;
+   public int cardSelected2;
 
+   public bool inSelection1, inSelection2;
+
+   float lastAimX1;
+   float lastAimX2;
+   
+   
+   public PlayerInputing playerInput;
+   
+
+   private void OnDisable()
+   {
+      GameManager.instance.players[0].OnClicked -= Player1Click;
+      GameManager.instance.players[1].OnClicked -= Player2Click;
+   }
+
+   private void Player1Click()
+   {
+      Debug.Log("mmh");
+      ChoseCard(cardSelected1);
+   }
+   
+   private void Player2Click()
+   {
+      Debug.Log("mmh2");
+      ChoseCard(cardSelected2+3);
+   }
+   public void Update()
+   {
+      if (inSelection1)
+      {
+         float x = GameManager.instance.players[0].aimInput.x;
+
+         if (x > 0.9f && lastAimX1 <= 0.9f)
+            cardSelected1++;
+         else if (x < -0.9f && lastAimX1 >= -0.9f)
+            cardSelected1--;
+
+         cardSelected1 = Mathf.Clamp(cardSelected1, 0, 2);
+         UpdateCardSelected();
+
+         lastAimX1 = x;
+      }
+
+      if (inSelection2)
+      {
+         float x = GameManager.instance.players[1].aimInput.x;
+
+         if (x > 0.9f && lastAimX2 <= 0.9f)
+            cardSelected2++;
+         else if (x < -0.9f && lastAimX2 >= -0.9f)
+            cardSelected2--;
+
+         cardSelected2 = Mathf.Clamp(cardSelected2, 0, 2);
+         UpdateCardSelected();
+
+         lastAimX2 = x;
+      }
+   }
+
+   public void UpdateCardSelected()
+   {
+      for (int i = 0; i < 3; i++)
+      {
+         cardsUI1[i].face.color = Color.white;
+         cardsUI2[i].face.color = Color.white;
+      }
+      
+      if(cardSelected1 != -1) cardsUI1[cardSelected1].face.color = new Color(0.3f, 0.3f, 0.3f);
+      if(cardSelected2 != -1)cardsUI2[cardSelected2].face.color = new Color(0.3f, 0.3f, 0.3f);
+   }
+   
 
    public void Launchmamere()
    {
@@ -29,7 +104,14 @@ public class CardChoice : MonoBehaviour
    
    public void ResolveMiniGameResults(int miniGame1, int miniGame2)
    {
+      inSelection1 = true;
+      inSelection2 = true;
       
+      GameManager.instance.players[0].OnClicked += Player1Click;
+      GameManager.instance.players[1].OnClicked += Player2Click;
+
+      cardSelected1 = 0;
+      cardSelected2 = 0;
       ResolvePlayer(miniGame1, ref player1Fail, ref cardsID1, cardsUI1);
       ResolvePlayer(miniGame2, ref player2Fail, ref cardsID2, cardsUI2);
       
@@ -72,16 +154,15 @@ public class CardChoice : MonoBehaviour
       bool isPlayer1 = ID <= 2;
       if (isPlayer1)
       {
-         
          if(ID ==0) animatorCards1.SetTrigger("1");
          if(ID ==1) animatorCards1.SetTrigger("2");
          if(ID ==2) animatorCards1.SetTrigger("3");
-         
+         inSelection1 = false;
          placementPlayer1.StartPlacement(cardsID1[ID]);
       }
       else
       {
-         
+         inSelection2 = false;
          if(ID ==3) animatorCards2.SetTrigger("1");
          if(ID ==4) animatorCards2.SetTrigger("2");
          if(ID ==5) animatorCards2.SetTrigger("3");
